1a592a90782ad6d41ec9ad3e14dfaefb0f56db46
nicocappabianca <nicolas.cappabianca@gmail.com>
2021-03-18 17:41:20 -0300

    update AppHead

--- components/AppHead.tsx
+++ components/AppHead.tsx
@@ -10,6 +10,12 @@ const AppHead: FC = () => (
     {/* Tags */}
     <link rel="dns-prefetch" href={AppData.siteUrl} />
     <link rel="preconnect" href={AppData.siteUrl} />
+
+    {/* Favicon */}
+    <link
+      rel="icon"
+      href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚡</text></svg>"
+    />
   </Head>
 );
 

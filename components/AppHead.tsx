aa7b1756e654c6ea021b4f2e8be393af279750cd
nicocappabianca <nicolas.cappabianca@gmail.com>
2021-03-19 12:59:07 -0300

    update favicon

--- components/AppHead.tsx
+++ components/AppHead.tsx
@@ -12,10 +12,7 @@ const AppHead: FC = () => (
     <link rel="preconnect" href={AppData.siteUrl} />
 
     {/* Favicon */}
-    <link
-      rel="icon"
-      href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚡</text></svg>"
-    />
+    <link rel="icon" type="image/svg+xml" href="/icons/favicon.ico" />
   </Head>
 );
 
